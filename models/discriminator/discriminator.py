dbb688c903bf2f1ff1168cd3c3be7899e2a272c2
Arturo0911 <anegreiross@outlook.com>
2021-11-06 18:09:17 -0500

    ok

--- models/discriminator/discriminator.py
+++ models/discriminator/discriminator.py
@@ -1,10 +1,19 @@
 """Discriminator module
 The goal for discriminator module:
+    Take an image and ouput wether or not it is
+    a real training image or fake image from the 
+    generator
 
+    Discriminative network gonna send numbers int 
+    the output by the flatten
 """
 
 from torch import nn
-from torch.nn import func
+from torch.nn import functional as F
 
 
-class Discriminatpor
+class Discriminator(nn.Module):
+    
+    def __init__(self, input_nc: int):
+        super(Discriminator, self).__init__()
+
