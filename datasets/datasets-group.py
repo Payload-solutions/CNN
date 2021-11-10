8e8c98874c9c279663101b6555f6baf8f2ddae39
Arturo0911 <anegreiross@outlook.com>
2021-11-10 00:52:25 -0500

    ok

--- datasets/datasets-group.py
+++ datasets/datasets-group.py
@@ -3,17 +3,38 @@ datasets of train and test, it's neccessary to match single values
 with values of the numeric dataset"""
 
 
-import pandas as pd
-import numpy as np
+from pprint import pprint
 import imageio
 from PIL import Image
+import os
 
-BASE = "milk-properties.csv"
 
-def asign_values_images():
-    data = pd.read_csv(BASE)
-    print(data)
+"""def asign_values_images():
+    base_dir = "train_bacteria/"
+    locate_dir = "image_train"
+    files = os.listdir(base_dir)
+
+    for num, x in enumerate(files):
+        image = Image.open(os.path.join(base_dir, x))
+        # print(image.format)
+        image.save(f'{locate_dir}/{num}_bacteria.png')"""
+
+
+
+def load_image_size():
+    base_dir = "test_bacteria/"
+    locate_dir = "image_test"
+    files = os.listdir(base_dir)
+
+    for num, x in enumerate(files):
+        image = Image.open(os.path.join(base_dir, x))
+        # print(image.size)
+        new_image = image.resize((64,64))
+        new_image.save(f'{locate_dir}/{num}_bacteria.png')
+
+
 
 
 if __name__ == "__main__":
-    asign_values_images()
+    # asign_values_images()
+    load_image_size()
