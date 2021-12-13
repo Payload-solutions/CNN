8c4646239ce5601157d3a9345c6bb4856438c6c3
Arturo0911 <anegreiross@outlook.com>
2021-12-13 15:38:24 -0500

    First training with tensorflow

--- cnn-tensorflow/main.py
+++ cnn-tensorflow/main.py
@@ -28,5 +28,39 @@ def main():
     train_image = train_image.reshape(train_image.shape[0], 28,28,1)
     test_images = test_images.reshape(test_images.shape[0], 28,28,1)
 
+    print(train_labels[9])
+
+    train_labels = tf.keras.utils.to_categorical(train_labels, 10)
+    test_labels = tf.keras.utils.to_categorical(test_labels, 10)
+
+    # crating model convolutional
+    model = tf.keras.Sequential()
+
+    model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+
+    model.summary()
+
+
+    # compile and training
+
+    model.compile(loss='categorical_crossentropy',
+        optimizer='rmsprop',
+        metrics=['accuracy']
+    )
+
+    model.fit(train_image, train_labels, batch_size=64, epochs=10)
+
+    print(model.evalutate(test_images, test_labels, verbose=0))
+
 if __name__ == "__main__":
     main()
