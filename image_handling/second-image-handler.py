f6da058ea1a5e66c7e97e6103da80b5f03e2917a
Arturo0911 <anegreiross@outlook.com>
2021-12-19 18:35:33 -0500

    updating knowledge about convolution and implementing in simples tensors

---
+++ image_handling/second-image-handler.py
@@ -0,0 +1,37 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import (
+    io,
+    color
+)
+import os
+import scipy.ndimage as nd
+
+
+if __name__ == "__main__":
+    
+    list_images = [x for x in os.listdir("../datasets/temp/image_train/")]
+
+    for x in list_images:
+
+        image = io.imread(os.path.join("../datasets/temp/image_train/", x))
+
+        print(image.shape)
+
+        # transforming to gray scale
+
+        image_gray = color.rgb2gray(image)
+        print(image_gray)
+        print(image_gray.shape)
+
+        kernel = np.array([
+            [-1, 0, 1],
+            [-1, 0, 1],
+            [-1, 0, 1]])
+
+        new_image = nd.convolve(image_gray, kernel)
+
+        print(new_image)
+        print(new_image.shape)
+        break
