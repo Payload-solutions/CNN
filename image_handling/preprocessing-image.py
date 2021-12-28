b7f7568a0375ae497257408e9c35dfee1aec8954
Arturo0911 <anegreiross@outlook.com>
2021-12-28 18:59:33 -0500

    ok

--- image_handling/preprocessing-image.py
+++ image_handling/preprocessing-image.py
@@ -38,20 +38,46 @@ def image_handler():
 
         img = img.reshape((1,)+img.shape)
         print(img.shape)
-        for batch in datage.flow(img, batch_size=1):
-            print(batch)
-            print(len(batch))
+
+
+        counter = 0
+        
+        
+        for batch in datage.flow(img, batch_size=2):
+            counter += 1
+            # print(batch)
+            #print(len(batch))
             img_plot = array_to_img(batch[0])
-            plt.figure(1)
-            plt.imshow(img_plot)
-            print(type(img_plot))
-            plt.show()
-            break
+            # print(dir(img_plot))
+            # plt.figure(1)
+            # plt.imshow(img_plot)
+            # print(type(img_plot))
+            # plt.show()
+
+            img_plot.save(f"../wherever/wherever_{counter}.png")
+            
+
+            if counter == 9:
+                break
+            
         break        
 
-def main():
-    image_handler()
 
+def watching_shapes():
+    
+    list_values = [x for x in os.listdir("../wherever")]
+
+    for x in list_values:
+
+        img = load_img("../wherever/"+x)
+        img = img_to_array(img)
+        print(img.shape)
+
+
+
+def main():
+    # image_handler()
+    watching_shapes()
 
 if __name__ == "__main__":
     main()
