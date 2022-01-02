9f27d2fea159c7134c240fd3d2fff3cc293f28fe
Arturo0911 <anegreiross@outlook.com>
2022-01-01 19:11:14 -0500

    script done

--- image_handling/renaming_images.py
+++ image_handling/renaming_images.py
@@ -4,25 +4,30 @@ to make the classification categorical, will be
 more easy to catch for the neural architecture
 """
 
+import re
 import os
 import sys
+import pandas as pd
 
 
+def renaiming_images(file_path: str, name: str) -> None:
 
-def renaiming_images(file_path: str) -> None:
-    
-    new_path = "../"
+    dataset = pd.read_csv("../model_images/milk-properties.csv")
 
-
+    files = os.listdir(file_path)
+    for x in files:
+        value = re.findall(r"\d{1,5}", x)
+        new_name = "_".join(dataset.iloc[int(value[0])]["quality_product"].split())
+        os.rename(file_path+x, f"{new_name}.png")
 
 
 def main():
     
-    train_path = ""
+    train_path = "../model_images/train/"
     test_path = ""
     valid_path = ""
 
-    renaiming_images(train_path)
+    renaiming_images(train_path, "train")
 
 
 
